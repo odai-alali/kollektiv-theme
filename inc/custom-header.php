c84642a8c0a331876b9696984db6465f127b66bc
Odai Alali <odai.alali@gmail.com>
2018-01-17 03:04:33 +0100

    add option for header bg colot

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -19,10 +19,7 @@
 function kollektiv_custom_header_setup() {
 	add_theme_support( 'custom-header', apply_filters( 'kollektiv_custom_header_args', array(
 		'default-image'          => '',
-		'default-text-color'     => '000000',
-		'width'                  => 1000,
-		'height'                 => 250,
-		'flex-height'            => true,
+		'default-bg-color'     => '#1e1e1e',
 		'wp-head-callback'       => 'kollektiv_header_style',
 	) ) );
 }
@@ -35,37 +32,22 @@ if ( ! function_exists( 'kollektiv_header_style' ) ) :
 	 * @see kollektiv_custom_header_setup().
 	 */
 	function kollektiv_header_style() {
-		$header_text_color = get_header_textcolor();
+		$header_bg_color = get_theme_mod('header_background_color');
 
 		/*
 		 * If no custom options for text are set, let's bail.
-		 * get_header_textcolor() options: Any hex value, 'blank' to hide text. Default: add_theme_support( 'custom-header' ).
+		 * get_header_bgcolor() options: Any hex value, 'blank' to hide text. Default: add_theme_support( 'custom-header' ).
 		 */
-		if ( get_theme_support( 'custom-header', 'default-text-color' ) === $header_text_color ) {
+		if ( get_theme_support( 'custom-header', 'default-bg-color' ) === $header_bg_color ) {
 			return;
 		}
 
 		// If we get this far, we have custom styles. Let's do this.
 		?>
 		<style type="text/css">
-		<?php
-		// Has the text been hidden?
-		if ( ! display_header_text() ) :
-		?>
-			.site-title,
-			.site-description {
-				position: absolute;
-				clip: rect(1px, 1px, 1px, 1px);
+			.site-header {
+				background-color: <?php echo esc_attr( $header_bg_color ); ?>;
 			}
-		<?php
-			// If the user has set a custom color for the text use that.
-			else :
-		?>
-			.site-title a,
-			.site-description {
-				color: #<?php echo esc_attr( $header_text_color ); ?>;
-			}
-		<?php endif; ?>
 		</style>
 		<?php
 	}
--- inc/customizer.php
+++ inc/customizer.php
@@ -11,40 +11,24 @@
  * @param WP_Customize_Manager $wp_customize Theme Customizer object.
  */
 function kollektiv_customize_register( $wp_customize ) {
-	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
-	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
-	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+	$wp_customize->remove_control('header_textcolor');
 
-	if ( isset( $wp_customize->selective_refresh ) ) {
-		$wp_customize->selective_refresh->add_partial( 'blogname', array(
-			'selector'        => '.site-title a',
-			'render_callback' => 'kollektiv_customize_partial_blogname',
-		) );
-		$wp_customize->selective_refresh->add_partial( 'blogdescription', array(
-			'selector'        => '.site-description',
-			'render_callback' => 'kollektiv_customize_partial_blogdescription',
-		) );
-	}
+	$wp_customize->add_setting('header_background_color', [
+		'default' => '#1e1e1e',
+    'transport'   => 'refresh',
+	]);
+	$wp_customize->add_control(
+		new WP_Customize_Color_Control(
+			$wp_customize,
+			'header_background_color',
+			[
+				'label'        => 'Header Background Color',
+				'section'    => 'colors',
+				'settings'   => 'header_background_color',
+			])
+		);
 }
 add_action( 'customize_register', 'kollektiv_customize_register' );
-
-/**
- * Render the site title for the selective refresh partial.
- *
- * @return void
- */
-function kollektiv_customize_partial_blogname() {
-	bloginfo( 'name' );
-}
-
-/**
- * Render the site tagline for the selective refresh partial.
- *
- * @return void
- */
-function kollektiv_customize_partial_blogdescription() {
-	bloginfo( 'description' );
-}
 
 /**
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
